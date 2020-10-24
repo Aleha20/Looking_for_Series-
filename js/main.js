0eb435fa7ec082f2ba1c377c2d4ce02bb1459332
Alejandra Calderon <alejandracsanchez7@gmail.com>
2020-10-24 21:13:38 +0200

    Addlistener function

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 
 const typeSerieName = document.querySelector('.js-input');
 const button = document.querySelector('.js-btn');
-let serieSearched = document.querySelector('.js-series__list');
+let serieSearched = document.querySelector('.js__full-series');
 let favoriteSeries = document.querySelector('.js-favorite__series');
 
 let series = [];
@@ -15,9 +15,7 @@ function getApiSeries() {
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
       series = data;
-      console.log(series);
       paintSeriesImage();
       setLocalStorage();
       getLocalStorage();
@@ -26,36 +24,71 @@ function getApiSeries() {
 function paintSeriesImage() {
   serieSearched.innerHTML = '';
   let paintHTML = ' ';
+  //   debugger;
 
   for (const serie of series) {
-    paintHTML += `<div class="js-series__list"><li>${serie.show.name}`;
-    paintHTML += `<img src="${serie.show.image.medium}"/>`;
-    paintHTML += `</li></div>`;
+    console.log('entra por aqui');
+    if (serie.show.image !== null) {
+      console.log('No soy dexter');
+      paintHTML += `<li class="js-series__list full__series"><span>${serie.show.name}</span>`;
+      paintHTML += `<img class="img__color" src="${serie.show.image.medium}"/>`;
+      paintHTML += `</li>`;
+    } else {
+      paintHTML += `<li class="js-series__list"><span>${serie.show.name}</span>`;
+      paintHTML += `<img class="img__color" src="https://via.placeholder.com/210x295/ffffff/666666/?
+        text=TV"/>`;
+      paintHTML += `</li>`;
+      console.log('soy dexter');
+    }
   }
   serieSearched.innerHTML = serieSearched.innerHTML + paintHTML;
+  addListener();
 }
 
 button.addEventListener('click', getApiSeries);
+let favoriteShow = [];
+
+function favoriteSerie(ev) {
+  console.log('entro y no hagooo nada');
+
+  let clicked = ev.currentTarget;
+
+  if (clicked === true) {
+    eachSerie.classlist.add('img__clicked');
+  } else {
+    eachSerie.classlist.add('img__color');
+  }
+  //   console.log(favoriteShow);
+  //   eachSerie.push(favoriteShow);
+
+  //   //   let favoriteShow = ev.currentTarget.id;
+  //   let favoriteShow = series[i];
+  //   if (favoriteSerieHTML === clicked) {
+  //     favoriteSerieHTML = true;
+  //   } else {
+  //     favoriteSerieHTML = false;
+  //   }
+}
+// favoriteSerie();
+
+function addListener() {
+  //convierto en una lista clicable y
+  let clickedImage = document.querySelectorAll('.js-series__list');
+
+  for (const eachSerie of clickedImage) {
+    eachSerie.addEventListener('click', favoriteSerie);
+    console.log('Estoy añadiendo el evento');
+  }
+}
 
 function setLocalStorage() {
-  localStorage.setItem('typeData2', series);
   const jsonData = JSON.stringify(series);
-
+  localStorage.setItem('typeData2', jsonData);
   //   console.log('entra a recoger datos');
 }
 
 function getLocalStorage() {
-  console.log('traigo los datos guardados');
-
-  localStorage.getItem('typeData2');
-  const localData = JSON.parse(jsonData);
+  //   console.log('traigo los datos guardados');
+  const localDataA = localStorage.getItem('typeData2');
+  const localDataB = JSON.parse(localDataA);
 }
-
-// if (series[i] === null) {
-//     paintHTML = `<img
-//           src="https://via.placeholder.com/210x295/ffffff/666666/?
-//       text=TV"
-//         />`;
-//   } else {
-//     paintHTML = `<img src="series[i].show.image.medium" />`;
-//   }
